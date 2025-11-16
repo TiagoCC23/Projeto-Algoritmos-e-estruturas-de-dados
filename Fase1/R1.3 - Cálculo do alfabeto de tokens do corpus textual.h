ffc7a95e1150c858aa70b4c6f3515d8f2c3cc833
TiagoCC23 <2024116801@ufp.edu.pt>
2025-11-16 15:40:56 +0000

    Coleta de caracteres feita com sucesso
    Aconteceu um bug sinistro ao usar dentro do ciclo for caracteresUnicos != '0', então teve de ser usado um strlen que faz a mesma coisa e o bug não faz sentido nenhum

--- Fase1/R1.3 - Cálculo do alfabeto de tokens do corpus textual.h
+++ Fase1/R1.3 - Cálculo do alfabeto de tokens do corpus textual.h
@@ -9,4 +9,20 @@
 #include <string.h>
 
 
+
+/**
+ *
+ * @param lines numero de linhas na matriz
+ * @param columns numero de colunas na matriz
+ * @param matr matriz de apontadores para strings
+ * @return
+ */
+
+char *caracteresUnicos(int lines, int columns, char *matr[lines][columns]);
+
+void analisarCaracteres(int lines, int columns, char *matr[lines][columns], int CaracteresAnalisados[256]);
+
+void testeColeta();
+
+
 #endif //PROJETO_ALGORITMOS_E_ESTRUTURAS_DE_DADOS_R1_3_CÁLCULO_DO_ALFABETO_DE_TOKENS_DO_CORPUS_TEXTUAL_H
