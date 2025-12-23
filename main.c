df1d03eca75f1faf4edf200f157a975d9e4eb524
Tiago Costa Chousal <2024116801@ufp.edu.pt>
2025-12-23 16:11:52 +0000

    FASE 1 FEITAAAAA
    Só falta alguma documentação, mas a parte prática tá toda

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include "Fase1/r1_3_alphabet_tokens.h"
 #include "r1_4_tokenization.h"
 #include "Fase1/r1_6_similarity.h"
-#include "Fase2/"
+#include "Fase2/r2_1_linked_list.h"
 #include "Fase2/r2_3_text_files.h"
 #include "Fase2/r2_4_binary_files.h"
 
@@ -11,20 +11,20 @@ int main(void){
      printf("FASE 1:\n");
      // printf("\n>>> R1.1 <<<\n");
      // test_r1_1();
-      printf("\n>>> R1.2 <<<\n");
-      test_r1_2();
+      // printf("\n>>> R1.2 <<<\n");
+      // test_r1_2();
     // printf("\n>>> R1.3 <<<\n");
     // test_r1_3();
     // printf("\n>>> R1.4 <<<\n");
      // test_r1_4();
     //printf("\n>>> R1.5 <<<\n");
     // teste_r1_5();
-    // printf("\n>>> R1.6 <<<\n");
-    // test_r1_6();
-    printf("FASE 2:\n");
-    printf("\n>>> R1.3:\n");
-    test_r2_1();
+    printf("\n>>> R1.6 <<<\n");
+    test_r1_6();
+    // printf("FASE 2:\n");
+    // printf("\n>>> R2.1:\n");
+    // test_r2_1();
     // printf("\n>>> R2.3 <<<\n");
-    test_r2_r3();
+    // test_r2_r3();
     return 0;
 }
